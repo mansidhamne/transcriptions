ddb1e7613eca6ea8ddf4ba7578573a936e1d9146
mansayy <mansiadhamne@gmail.com>
2025-01-17 14:50:32 +0530

    model option

--- app.py
+++ app.py
@@ -2,23 +2,25 @@ import streamlit as st
 import tempfile
 import whisper
 
-def transcribe_audio(file):
+def transcribe_audio(file, model="tiny"):
+    whisper_model = whisper.load_model(model)
     with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
         tmp_file.write(file.read())
         tmp_file_path = tmp_file.name
     with st.spinner('Transcribing...'):
-        model = whisper.load_model("base")
-        result = model.transcribe(tmp_file_path)
- 
+        result = whisper_model.transcribe(tmp_file_path, fp16=False)
+
     return result["text"]
 
 st.title("Speech to Text: Story Transcriptions")
 
 uploaded_file = st.file_uploader("Upload an audio file", type=["mp3", "wav", "m4a"])
 
+model = st.selectbox("Choose Whisper Model", ["tiny", "base"])
+
 if uploaded_file is not None:
     st.audio(uploaded_file, format='audio/mp3')
-    transcript = transcribe_audio(uploaded_file)
+    transcript = transcribe_audio(uploaded_file, model)
     st.write("Transcript:")
     st.write(transcript)
     st.download_button(
@@ -27,4 +29,5 @@ if uploaded_file is not None:
         file_name="transcript.txt",
         mime="text/plain"
     )
+
 
